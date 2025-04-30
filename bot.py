0d692f9d5852dfcb8a51d8315ea94ff39cf20fdb
botkomakers <siam420riiya@gmail.com>
2025-04-30 15:14:32 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,3 +44,24 @@ async def logo(_, m: Message):
 bot.db = {}
 
 bot.run()
+
+
+
+
+
+# Dummy HTTP server for Render port binding
+import os
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run).start()
